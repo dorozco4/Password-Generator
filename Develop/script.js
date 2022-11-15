4fced1c30f456a048a45a909025c71ac7f08246a
David Orozco <davorozco4@gmail.com>
2022-11-15 15:11:35 -0500

    commiting changes, adding if statemnts, more variables, etc

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    max = min 
+    min = 0
+ }
+ var rand = Math.random()
+ return Math.floor(min*(1 - rand) + rand * max)
+}
+function getRandomItem(list) {
+
+  return list[randomInt(list.length)]
+}
+
 
 
 function generatePassword() {
  
+// started the window inputs by asking the user questions.
+// then depending on the answer they give us, they get whether its too short or too long.
+
+
 var signInput = window.prompt("How long should the password be?")
 
 var passwordLength = parseInt(signInput)
-
-var confirmNumbers= window.confirm("Would you like to add numbers?")
-var confirmUppercase= window.confirm("Would you like to add upper case letters?")
-var confirmLowercase= window.confirm("Would you like to add lower case letters?")
-var confirmSymbols= window.confirm("Would you like to add symbol(s)?")
 
 
 if (isNaN(passwordLength)) {
@@ -21,20 +33,80 @@ if (isNaN(passwordLength)) {
 } 
   
 if (passwordLength < 8 || passwordLength > 128) {
-    window.alert(" Password is too short or not long enough")
+    window.alert(" Password is too short or too long enough")
     return
 }
 
+// then added a few more windows by asking if they wanted the numbers, upper/lower cased letters, and/or symboles.
+
+
+var confirmNumbers= window.confirm("Would you like to add numbers?")
+var confirmUppercase= window.confirm("Would you like to add upper case letters?")
+var confirmLowercase= window.confirm("Would you like to add lower case letters?")
+var confirmSymbols= window.confirm("Would you like to add symbol(s)?")
+
+
+var numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+var upperCase = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+var lowerCase = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+'w', 'x', 'y','z')
+var symbolsS = ('!','@','#','$','%','^','*','?')
+
+
+// had to make a box inside of another box for the numbers/letters/symbols
+
+var randomItems = []
+
+if (confirmNumbers == true) {
+  randomItems.push(numbers)
+}
+if (confirmUppercase == true) {
+  randomItems.push(upperCase)
+}
+if (confirmLowercase == true) {
+  randomItems.push(lowerCase) 
+} 
+if (confirmSymbols == true) {
+  randomItems.push(symbolsS)
+}
+
+
+// function randomInt(min, max) {
+//   if (!max) {
+//     max = min 
+//     min = 0
+//  }
+//  var rand = Math.random()
+//  return Math.floor(min*(1 - rand) + rand * max)
+// }
+// function getRandomItem(list) {
+
+//   return list[randomInt(list.length)]
+// }
+
+
+
+
+var generatePassword = "" 
+
+
+for (var i = 0; i < passwordLength; i++){ 
+  var randomList = getRandomItem(randomItems) 
+  var randomchar = getRandomItem(randomList)
+  generatePassword += randomchar
+}
+return generatePassword
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-n 
+ 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
+

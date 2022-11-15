6bead2418e6819b1b710ad1c273ce85678d17303
David Orozco <davorozco4@gmail.com>
2022-11-15 12:39:15 -0500

    Adding funtions and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector('#password')
+
 
 
 function generatePassword() {
-  console.log("Password has been entered!")
+ 
+var signInput = window.prompt("How long should the password be?")
+
+var passwordLength = parseInt(signInput)
+
+var confirmNumbers= window.confirm("Would you like to add numbers?")
+var confirmUppercase= window.confirm("Would you like to add upper case letters?")
+var confirmLowercase= window.confirm("Would you like to add lower case letters?")
+var confirmSymbols= window.confirm("Would you like to add symbol(s)?")
+
+
+if (isNaN(passwordLength)) {
+  window.alert("This is not a number!")
+  return
+} 
   
-
-
-  return "Password goes here"
+if (passwordLength < 8 || passwordLength > 128) {
+    window.alert(" Password is too short or not long enough")
+    return
 }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+n 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- 
+generateBtn.addEventListener("click", writePassword)
